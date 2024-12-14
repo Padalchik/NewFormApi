8d381a3d56a4db0ba385a681cce102ad7b474f36
Padalchik <andrey_nikiforov_1998@mail.ru>
2024-12-14 18:23:34 +0300

    Create Profile service

---
+++ Services/ProfileService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebForm.Entity;
+
+namespace WebForm.Services
+{
+    public class ProfileService
+    {
+        private readonly ApplicationDBContext _dbContext;
+
+        public ProfileService(ApplicationDBContext context)
+        {
+            _dbContext = context;
+        }
+
+        /// <summary>
+        /// Возвращает список всех анкет
+        /// </summary>
+        /// <returns>Список анкет.</returns>
+        public async Task<List<Profile>> GetProfiles()
+        {
+            var profiles = await _dbContext.Profiles.ToListAsync();
+            return profiles;
+        }
+    }
+}
